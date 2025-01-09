75fbbc3c1bfd10a8fe82ca44ef1cf5ea834bd406
andrewDzokwom <andrewa441@gmail.com>
2025-01-09 18:38:19 +0100

    add frontend (dist dir)

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
-
+app.use(cors())
 // to handle incoming objects in JSON
 
 app.use(express.json())
@@ -81,8 +82,8 @@ app.post("/api/notes", (req, res)=>{
   notes = notes.concat(note)
   res.json(note)
 })
-
-app.listen(3001, ()=>{
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=>{
     console.log(`Seerver running on address http://localhost:3001`);
     
 })
