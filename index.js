22c07eecedf6e85cf3f2c09c19ea8e8590470a2a
andrewDzokwom <andrewa441@gmail.com>
2025-01-19 16:30:19 +0100

    fix issue with the PORT #

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post("/api/notes", (req, res)=>{
   notes = notes.concat(note)
   res.json(note)
 })
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`Seerver running on address http://localhost:${PORT}`);
     
