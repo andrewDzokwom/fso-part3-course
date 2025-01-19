04d179639e3bbd2a78e0d4ff18b128b739bab4e6
andrewDzokwom <andrewa441@gmail.com>
2025-01-19 16:18:06 +0100

    fix typo

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ app.get("/", (req, res)=>{
     res.send(`<h1>This is the homepage</h1>`)
 })
 
-// app.get("/api/notes/:id", (req, res)=>{
-//     const id = req.params.id
-//     const note = notes.find(note=> note.id === id)
-//     if (note){
-//         res.json(note)
-//         return
-//     }
-//     res.status(400).end(JSON.stringify({
-//         error: "Note not found"
-//     }))
+app.get("/api/notes/:id", (req, res)=>{
+    const id = req.params.id
+    const note = notes.find(note=> note.id === id)
+    if (note){
+        res.json(note)
+        return
+    }
+    res.status(400).end(JSON.stringify({
+        error: "Note not found"
+    }))
     
-// })
+})
 
 app.get("/api/notes", (req, res)=>{
   Note.find({}).then(notes => {
@@ -40,36 +40,44 @@ app.get("/api/notes", (req, res)=>{
 
 //delete handling
 
-// app.delete("/api/notes/:id", (req, res)=>{
-//   const UserId = req.params.id;
-//   notes = notes.filter(note => note.id !== UserId);
-//   res.status(204).end()
-// })
+app.delete("/api/notes/:id", (req, res)=>{
+  const UserId = req.params.id;
+  notes = notes.filter(note => note.id !== UserId);
+  res.status(204).end()
+})
 
-// // id generation
-// const getId = ()=>{
-//   const maxId = notes.length > 0? Math.max(...notes.map(note => Number(note.id))): 0;
-//   return maxId + 1
-// }
-// // post handlind 
-// app.post("/api/notes", (req, res)=>{
-//   console.log(req.body)
-//   const body = req.body
-//   if (!body.content){
-//     return res.status(400).json({
-//       error: "content missing"
-//     })
-//   }
+// id generation
+const getId = ()=>{
+  const maxId = notes.length > 0? Math.max(...notes.map(note => Number(note.id))): 0;
+  return maxId + 1
+}
+// post handlind 
+app.post("/api/notes", (req, res)=>{
+  console.log(req.body)
+  const body = req.body
+  if (!body.content){
+    return res.status(400).json({
+      error: "content missing"
+    })
+  }
 
-//   const note = {
-//     content: body.content,
-//     important: Boolean(body.important) || false,
-//     id: getId()
-//   }
-
-//   notes = notes.concat(note)
-//   res.json(note)
-// })
+  const note = new Note({
+    content: body.content,
+    important: Boolean(body.important) || false
+  })
+  
+  // {
+  //   content: body.content,
+  //   important: Boolean(body.important) || false,
+  //   id: getId()
+  // }
+  note.save().then(savedNote => {
+    res.json(savedNoite)
+  })
+  
+  notes = notes.concat(note)
+  res.json(note)
+})
 const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`Seerver running on address http://localhost:3001`);
